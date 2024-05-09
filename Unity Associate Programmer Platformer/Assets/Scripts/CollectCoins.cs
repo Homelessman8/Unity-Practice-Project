6f74acf84074e7181e6730a75fc1d437b83fb22c
Homelessman8 <sookram1208@gmail.com>
2024-05-09 22:50:38 +1000

    Added Project to Repo
    Also Completed Course 1 Session 1:
    
    - Added player health state
    - added a mechanism for updating health state
    - added a text UI to see health state updating

---
+++ Unity Associate Programmer Platformer/Assets/Scripts/CollectCoins.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoins : MonoBehaviour
+{
+    [Tooltip("The particles that appear after the player collects a coin.")]
+    public GameObject coinParticles;
+
+    PlayerMovement playerMovementScript;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerMovementScript = other.GetComponent<PlayerMovement>();
+            playerMovementScript.soundManager.PlayCoinSound();
+            ScoreManager.score += 10;
+            //add the health
+            if (playerMovementScript.playerStats.health < 100)
+            {
+                playerMovementScript.ChangeHealth(1);
+            }
+            else
+            { 
+                playerMovementScript.ChangeHealth(0);
+            }
+            
+            GameObject particles = Instantiate(coinParticles, transform.position, new Quaternion());
+            Destroy(gameObject);
+        }
+    }
+}
