f72e6bdfedf392848b1a606f854aaabb49231c4b
Manjeeve <123073449+Homelessman8@users.noreply.github.com>
2024-05-12 13:24:29 +1000

    Practice and learned Refactoring Code
    Goal is to create more legible and flexible code, without messing up the original functions.
    
    Private variables can be accessed only from the class which they belong to.

--- Unity Associate Programmer Platformer/Assets/Scripts/RefactorEnemy.cs
+++ Unity Associate Programmer Platformer/Assets/Scripts/RefactorEnemy.cs
@@ -4,27 +4,6 @@ using UnityEngine;
 
 public class RefactorEnemy : MonoBehaviour
 {
-    public Stats enemyStats;
-
-    [Tooltip("The transform that will lock onto the player once the enemy has spotted them.")]
-    public Transform sight;
-
-    [Tooltip("The transform to which the enemy will pace back and forth to.")]
-    public Transform[] patrolPoints;
-
-    [Tooltip("Blue explosion particles")]
-    public GameObject enemyExplosionParticles;
-
-    public int currentPatrolPoint = 0;
-
-    public bool slipping = false;
-   
-    public float facing;
-    
-    public Rigidbody rb;
-
-    private GameObject player;
-
     /// <summary>
     /// Contains tunable parameters to tweak the enemy's movement and behavior.
     /// </summary>
@@ -40,57 +19,49 @@ public class RefactorEnemy : MonoBehaviour
 
         [Tooltip("How fast the enemy runs after the player (only when idle is false).")]
         public float chaseSpeed;
-
-        [Tooltip("Whether the enemy is idle or not. Once the player is within distance, idle will turn false and the enemy will chase the player.")]
-        public bool idle;
-
+                
         [Tooltip("How close the enemy needs to be to explode")]
         public float explodeDist;
 
     }
+
+    public Stats enemyStats;
+
+    [Tooltip("The transform that will lock onto the player once the enemy has spotted them.")]
+    public Transform sight;
+
+    [Tooltip("The transform to which the enemy will pace back and forth to.")]
+    public Transform[] patrolPoints;
+
+    [Tooltip("Blue explosion particles")]
+    public GameObject enemyExplosionParticles;
+
+    public int currentPatrolPoint = 0;
+
+    private bool idle = true;
+
+    private bool slipping = false;
+   
+    private float facing;
+    
+    private Rigidbody rb;
+
+    private GameObject player;
+
+   // private Patrol patrolBehaviour;
+
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+       // patrolBehaviour = GetComponent<Patrol>();
     }
     private void Update()
     {
-        // changes the enemy's behavior: pacing in circles or chasing the player
-        if (enemyStats.idle == true)
-        {
-            //Patrol Logic
-                Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
-                transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.walkSpeed * Time.deltaTime);
-
-                if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
-                {
-                    currentPatrolPoint++;
-                    if (currentPatrolPoint > patrolPoints.Length - 1)
-                    {
-                        currentPatrolPoint = 0;
-                    }
-                }
-        }
-        else if (enemyStats.idle == false)
-        {
-            //Chase the player
-             sight.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
-             transform.LookAt(sight);
-             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * enemyStats.chaseSpeed);
-           
-            //Explode if we get within the enemyStats.explodeDist
-            if (Vector3.Distance(transform.position, player.transform.position) < enemyStats.explodeDist)
-            {
-                StartCoroutine("Explode");
-                enemyStats.idle = true;
-            }
-        }
-
-        // stops enemy from following player up the inaccessible slopes
-        if (slipping == true)
-        {
-            transform.Translate(Vector3.back * 20 * Time.deltaTime, Space.World);
-        }
+        //Using method enemyBehaviour
+        enemyBehaviour();
     }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.layer == 9)
@@ -102,7 +73,6 @@ public class RefactorEnemy : MonoBehaviour
             slipping = false;
         }
     }
-
 
    private void OnTriggerEnter(Collider other)
     {
@@ -130,5 +100,43 @@ public class RefactorEnemy : MonoBehaviour
         Destroy(transform.parent.gameObject);
     }
 
+    private void enemyBehaviour()
+    {
+        // changes the enemy's behavior: pacing in circles or chasing the player
+        if (idle == true)
+        {
+            //Patrol Logic
+            Vector3 moveToPoint = patrolPoints[currentPatrolPoint].position;
+            transform.position = Vector3.MoveTowards(transform.position, moveToPoint, enemyStats.walkSpeed * Time.deltaTime);
 
+            if (Vector3.Distance(transform.position, moveToPoint) < 0.01f)
+            {
+                currentPatrolPoint++;
+                if (currentPatrolPoint > patrolPoints.Length - 1)
+                {
+                    currentPatrolPoint = 0;
+                }
+            }
+        }
+        else if (idle == false)
+        {
+            //Chase the player
+            sight.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+            transform.LookAt(sight);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * enemyStats.chaseSpeed);
+
+            //Explode if we get within the enemyStats.explodeDist
+            if (Vector3.Distance(transform.position, player.transform.position) < enemyStats.explodeDist)
+            {
+                StartCoroutine("Explode");
+                enemyStats.idle = true;
+            }
+        }
+
+        // stops enemy from following player up the inaccessible slopes
+        if (slipping == true)
+        {
+            transform.Translate(Vector3.back * 20 * Time.deltaTime, Space.World);
+        }
+    }
 }
